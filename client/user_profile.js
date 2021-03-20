b9c37abe6f73c7bf9dcda7ad488580c05f5e444d
FNocioni <79723571+FNocioni@users.noreply.github.com>
2021-03-19 23:19:25 -0500

    Add files via upload

--- client/user_profile.js
+++ client/user_profile.js
@@ -2,16 +2,38 @@ console.log("user_profile.js: Startup");
 
 let userData = [];
 
+
+const fullname = document.getElementById("flname");
+const add1 = document.getElementById("add1");
+const add2 = document.getElementById("add2");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const zip = document.getElementById("zip");
+const curr = window.localStorage.getItem("Current User");
+
+function confirmChanges(){
+    alert("Changes confirmed");
+
+    const body = { 
+        "username": curr,
+        "fullname": fullname.value,
+        "address1": add1.value,
+        "address2": add2.value,
+        "city": city.value,
+        "state": state.value,
+        "zipcode": zip.value 
+    };
+    const response = fetch("http://localhost:5000/confirmChanges", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    }
+    );
+
+    return true;
+}
+
 function populateFields(){
-    const fullname = document.getElementById("flname");
-    const add1 = document.getElementById("add1");
-    const add2 = document.getElementById("add2");
-    const city = document.getElementById("city");
-    const state = document.getElementById("state");
-    const zip = document.getElementById("zip");
-
-    const curr = window.localStorage.getItem("Current User");
-
     for(var i = 0; i < userData.length; i++){
         if(userData[i].username == curr){
             fullname.value = userData[i].fullname;
@@ -20,6 +42,7 @@ function populateFields(){
             city.value = userData[i].city;
             state.value = userData[i].state;
             zip.value = userData[i].zipcode;
+            return true;
         }
     }
 }
